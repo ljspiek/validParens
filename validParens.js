f8ebf104d70f83c73f4b49150ceaa9e95cbc0bd8
Laura Spieker <ljspiek@gmail.com>
2020-07-28 20:53:12 -0500

    fixed edge cases

--- validParens.js
+++ validParens.js
@@ -1,5 +1,5 @@
 function validParens(parens) {
-  let output = false;
+  let output = true;
   let open = 0;
   for (let i = 0; i < parens.length; i++) {
     if (parens[i] === ")" && !open) {
@@ -7,12 +7,14 @@ function validParens(parens) {
       break;
     } else if (parens[i] === "(") {
       open++;
-    } else if (parens[i] === ")" && open !== 0) {
+    } else if (parens[i] === ")" && open > 0) {
       open--;
       output = true;
     }
   }
-  return output;
+  if (open === 0) {
+    return output;
+  } else return false;
 }
 
-console.log(validParens(")()"));
+console.log(validParens("()()(())()()(("));
